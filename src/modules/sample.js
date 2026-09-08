480fce4c61bc45d67a5a1289ceba316ed4a679ac
ysjeondev <hpc9909@gmail.com>
2026-09-08 11:25:41 +0900

    18장 완료 519쪽까지 완료 260908

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,92 +1,43 @@
 import { createAction, handleActions } from 'redux-actions';
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
-import { startLoading, finishLoading } from './loading';
+import createRequestSaga from '../lib/createRequestSaga';
 
-// 액션 타입
+//액션 타입
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
-
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-// 액션 생성 함수
+//액션 생성 함수
 export const getPost = createAction(
     GET_POST,
     id => id
 );
-
 export const getUsers = createAction(GET_USERS);
+    // API 요청 Saga 생성
+    const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+    const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
-// POST Saga
-function* getPostSaga(action) {
-    yield put(startLoading(GET_POST));
-
-    try {
-        const post = yield call(
-            api.getPost,
-            action.payload
-        );
-
-        yield put({
-            type: GET_POST_SUCCESS,
-            payload: post.data
-        });
-    } catch (e) {
-        yield put({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-        });
-    }
-
-    yield put(finishLoading(GET_POST));
-}
-
-// USERS Saga
-function* getUsersSaga() {
-    yield put(startLoading(GET_USERS));
-
-    try {
-        const users = yield call(api.getUsers);
-
-        yield put({
-            type: GET_USERS_SUCCESS,
-            payload: users.data
-        });
-    } catch (e) {
-        yield put({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-        });
-    }
-
-    yield put(finishLoading(GET_USERS));
-}
-
-// ★ modules/index.js가 찾고 있는 함수
-export function* sampleSaga() {
+    //Saga
+    export function* sampleSaga() {
     yield takeLatest(GET_POST, getPostSaga);
     yield takeLatest(GET_USERS, getUsersSaga);
 }
 
-// 초기 상태
+//초기 상태
 const initialState = {
     post: null,
     users: null
 };
 
-// reducer
+//Reducer
 const sample = handleActions(
     {
         [GET_POST_SUCCESS]: (state, action) => ({
             ...state,
             post: action.payload
         }),
-
         [GET_USERS_SUCCESS]: (state, action) => ({
             ...state,
             users: action.payload
